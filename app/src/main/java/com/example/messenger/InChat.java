d3237513071fd9bd5020bd271aef14692e19e3ef
Zezus <rustam-sadvakaso@mail.ru>
2018-03-19 20:53:47 +0600

    initial

---
+++ app/src/main/java/com/example/messenger/InChat.java
@@ -0,0 +1,13 @@
+package com.example.messenger;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class InChat extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_in_chat);
+    }
+}
